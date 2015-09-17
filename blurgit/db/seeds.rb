c21dd20f6055354a4d943547cc8ae4dc816aafeb
Jesse Wolgamott <jesse@comalproductions.com>
2015-09-17 10:03:16 -0500

    Add Articles, seed them titles

---
+++ blurgit/db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+5.times do
+  article = Article.new
+  article.title = Faker::Company.catch_phrase
+  article.save
+end
